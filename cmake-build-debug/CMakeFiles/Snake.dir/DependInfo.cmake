154f7f7e35f676989d4f7191a0286dc870445407
michmaz061 <56159010+michmaz061@users.noreply.github.com>
2022-12-13 12:18:29 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Snake.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Snake/head.cpp" "E:/Snake/cmake-build-debug/CMakeFiles/Snake.dir/head.cpp.obj"
+  "E:/Snake/main.cpp" "E:/Snake/cmake-build-debug/CMakeFiles/Snake.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Snake.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Snake.dir/head.cpp.obj"
+  "CMakeFiles/Snake.dir/main.cpp.obj"
+  "Snake.exe"
+  "Snake.exe.manifest"
+  "Snake.pdb"
+  "libSnake.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Snake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
